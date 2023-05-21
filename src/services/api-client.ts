4fdc1b531a0fcf4dea756f17d8ed882aabf53830
Abdiwali-Eid <cabdiwalibashir232@gmail.com>
2023-05-21 20:29:59 +0300

    new updates

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,36 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
-export default axios.create({
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'bf8c3935dba64b61a980cec564ce1f1c',
+    key: '2a3378777b694ff2a836dca17e3fcab7',
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
